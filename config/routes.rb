afcfecc25a2a3f36a3153aef9165bce3bf3d3d2a
Andreas Månsson <amanssons@gmail.com>
2019-06-04 15:54:11 +0200

    Rest completed with review feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [ :edit, :update, :destroy ] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # get "restaurants/:id", to: "restaurant#show"
+  # post "restaurants", to: "restaurants#create", as: :new_restaurant
+  # post "restaurants/:id/reviews", to: "reviews#create"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up all seeds."
+Restaurant.destroy_all
+
+puts "Adding new restaurants..."
+Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: "chinese")
+Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: "italian")
+Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: "italian")
+Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: "french")
+Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: "belgian")
+
+puts "Completed adding restaurants."
+
+
+# puts "Adding new reviews..."
+
+
+
+# puts "Completed adding reviews."
